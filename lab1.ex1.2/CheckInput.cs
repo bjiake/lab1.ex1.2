11d028e5fcfa1dc8965fe24c19605ea70b42d4a8
bjiake <suyd@list.ru>
2022-09-19 14:55:35 +0700

    финалочка

--- lab1.ex1.2/CheckInput.cs
+++ lab1.ex1.2/CheckInput.cs
@@ -2,40 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace lab1.ex1._2
 {
     internal class CheckInput
     {
-        public static void ChechInputWord(TextCompositionEventArgs e)
-        {
-            foreach (char c in e.Text)
-            {
+        public static void ChechInputWord(TextCompositionEventArgs e) { 
+            foreach (char c in e.Text) { 
                 if (
                     c < 'A' || c > 'Z' &&
                     c < 'a' || c > 'z' &&
                     c < 'А' || c > 'Я' &&
                     c < 'а' || c > 'я'
-                    )
-                {
+                    ) { 
                     e.Handled = true;
                     break;
                 }
             }
         }
 
-        public static void IsOnlyDigit(TextCompositionEventArgs e)
-        {
-            foreach (char c in e.Text)
-            {
-                if ( !Char.IsDigit(c) && c != ',' )
-                {
-                    e.Handled = true;
-                    break;
-                }
+        public static void IsOnlyDigit(TextCompositionEventArgs e, object sender) {
+
+            var textBox = sender as TextBox;
+            Regex regex = new Regex("^[^0-9\\,]+$");
+            if (
+                ( textBox.Text.Contains(',') && e.Text == "," ) ||
+                ( textBox.Text == "" && e.Text == "," )
+            ) {
+                e.Handled = true;
+            } else {
+                e.Handled = regex.IsMatch(e.Text);
             }
+
+            //foreach (char c in e.Text) {
+
+            //    if (!Char.IsDigit(c) && c != ',') {
+            //        e.Handled = true;
+            //        break;
+            //    }
+            //}
         }
     }
 }
--- lab1.ex1.2/MainWindow.xaml.cs
+++ lab1.ex1.2/MainWindow.xaml.cs
@@ -20,6 +20,11 @@ namespace lab1.ex1._2
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static int countLengthFraction = 0;
+        public static int countRadiusFraction = 0;
+        public static int countRadiusInternalFraction = 0;
+        public static int countDensityFraction = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,38 +32,39 @@ namespace lab1.ex1._2
 
         private void TbLength_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            CheckInput.IsOnlyDigit(e);
+
+            CheckInput.IsOnlyDigit(e, sender);
         }
 
         private void TbRadius_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            CheckInput.IsOnlyDigit(e);
+            CheckInput.IsOnlyDigit(e, sender);
         }
 
         private void TbInternalRadius_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            CheckInput.IsOnlyDigit(e);
+            CheckInput.IsOnlyDigit(e, sender);
         }
 
         private void tbDensity_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            CheckInput.IsOnlyDigit(e);
+            CheckInput.IsOnlyDigit(e, sender);
         }
 
         private void BtResult_Click(object sender, RoutedEventArgs e)
         {
             double volume, weigth;
-            bool parseLength = Double.TryParse(TbLength.Text, out var length);
-            bool parseRadius = Double.TryParse(TbRadius.Text, out var radius);
-            bool parseRadiusInternal = Double.TryParse(TbInternalRadius.Text, out var radiusInternal);
-            bool parseDensity = Double.TryParse(tbDensity.Text, out var density);
+            bool parseLength = Double.TryParse(TbLength.Text, out double length);
+            bool parseRadius = Double.TryParse(TbRadius.Text, out double radius);
+            bool parseRadiusInternal = Double.TryParse(TbInternalRadius.Text, out double radiusInternal);
+            bool parseDensity = Double.TryParse(tbDensity.Text, out double density);
 
             if (!parseLength || !parseDensity || !parseRadius || !parseRadiusInternal)
             {
                 MessageBox.Show("Введены некорректные данные");
                 return;
             }
-            if (length == 0 || radius == 0 || radiusInternal == 0 || density == 0 || radius < radiusInternal)//Не корректные данные
+            if (length == 0 || radius == 0 || radiusInternal == 0 || density == 0 || radius <= radiusInternal)//Не корректные данные
             {
                 TbLength.Text = "";
                 TbRadius.Text = "";
@@ -70,6 +76,15 @@ namespace lab1.ex1._2
 
             volume = length * Math.PI * (Math.Pow(radius, 2) * Math.Pow(radiusInternal, 2));
             weigth = volume * density;
+            int round = 6;
+
+            if(Math.Round(weigth, 6) == 0) {
+                while(Math.Round(weigth, round) == 0) {
+                    round++;
+                }
+                MessageBox.Show($"Масса трубы:{Math.Round((weigth), round)}\n Объем трубы равен: {Math.Round(( volume ), round + 2)}");
+                return;
+            }
 
             MessageBox.Show($"Масса трубы:{Math.Round((weigth), 6)}\n Объем трубы равен: {Math.Round((volume), 6)}");
         }
